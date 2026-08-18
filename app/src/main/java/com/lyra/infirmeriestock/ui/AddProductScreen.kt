4030da6d83af450ff458e7f30a4b417f44996419
drdiaz663 <drdiaz663@gmail.com>
2026-08-18 17:25:51 +0200

    Update AddProductScreen.kt

--- app/src/main/java/com/lyra/infirmeriestock/ui/AddProductScreen.kt
+++ app/src/main/java/com/lyra/infirmeriestock/ui/AddProductScreen.kt
@@ -1,5 +1,7 @@
 package com.lyra.infirmeriestock.ui
 
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
